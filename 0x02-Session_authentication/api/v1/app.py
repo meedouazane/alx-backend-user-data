10f9ef1d226c9143a37157366f2772c319ee671e
Ouazane <mohamedouazane@yahoo.com>
2024-05-22 16:14:31 -0700

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,8 +63,9 @@ def before_request():
             abort(401)
         if not auth.current_user(request):
             abort(403)
-        if auth.authorization_header(request) and auth.session_cookie(request):
-            return None, abort(401)
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
         request.current_user = auth.current_user(request)
 
 
