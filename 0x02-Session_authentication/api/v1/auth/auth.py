36fd1f5831ccefdc9ee9c1c3e874330621b021e6
Ouazane <mohamedouazane@yahoo.com>
2024-05-22 11:25:53 -0700

    all

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Auth class """
+import fnmatch
+import uuid
+from typing import TypeVar, List
+from flask import request
+import fnmatch
+from os import getenv
+
+
+class Auth:
+    """ class Auth """
+
+    def require_auth(
+            self, path: str, excluded_paths: List[str]) -> bool:
+        """ public method require auth """
+        i = 0
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        for item in excluded_paths:
+            if fnmatch.fnmatch(path, item):
+                return False
+        if path not in excluded_paths:
+            return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """ public method require authorization header """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ public method current user """
+        return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request """
+        if request is None:
+            return None
+        cookie_name = getenv('SESSION_NAME')
+        cookies = getattr(request, 'cookies', None)
+        return cookies.get(cookie_name)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Auth class """
+import uuid
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """  class SessionAuth that Create a session """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID"""
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_by_session_id(session_id)
+        return User.get(user_id)
