5b5d681aa88686a18aca6e308f1382fd3b3d68a8
Ouazane <mohamedouazane@yahoo.com>
2024-05-23 08:38:03 -0700

    8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -28,3 +28,14 @@ def Session_auth():
         session_name = getenv('SESSION_NAME')
         response.set_cookie(session_name, c_session)
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ deletes the user session / logout """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
