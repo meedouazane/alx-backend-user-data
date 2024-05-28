edd58d75a655da7b2873847af25cbf47f0bba15d
Ouazane <mohamedouazane@yahoo.com>
2024-05-28 13:11:27 -0700

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -117,10 +117,11 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed)
         except NoResultFound:
             raise ValueError
+        hashed = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed, reset_token=None)
 
 
 def _generate_uuid(self) -> str:
