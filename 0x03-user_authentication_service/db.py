3237268e19c4877b6cf22ece8a35d1211dec5e4e
Ouazane <mohamedouazane@yahoo.com>
2024-05-28 09:21:29 -0700

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,6 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.exc import NoResultFound
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import User
@@ -32,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ add and save user to database """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
