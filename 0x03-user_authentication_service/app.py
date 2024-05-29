232d842ccde0ed9a1027ee83ace180e4d939af59
Ouazane <mohamedouazane@yahoo.com>
2024-05-29 06:03:50 -0700

    put

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,7 +92,7 @@ def get_reset_password_token():
     abort(403)
 
 
-@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """ Update password end-point """
     email = request.form.get("email")
